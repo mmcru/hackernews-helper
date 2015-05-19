0c3db5b14a2430f99d8fe42b0d8468aea7040dcb
matt cru <mmcru@outlook.com>
2015-05-19 16:33:35 -0400

    added sort buttons, changed some formatting and colors, deleted extra files

--- data/panel-content.js
+++ data/panel-content.js
@@ -1,3 +1,17 @@
+currentTabUrl = "";
+
+
+//jquery in here
+
+$(document).ready(function() {
+	$(".unselected").click(function(){
+		window.alert(".unselected was clicked");
+		$( this ).toggleClass("sorter");
+	});
+});
+
+
+
 addDateToDateField = function addDateToDateField(apiId) {
 	 $.getJSON(
 		"http://hn.algolia.com/api/v1/items/" + apiId,
@@ -8,30 +22,34 @@ addDateToDateField = function addDateToDateField(apiId) {
  };
 
 
-self.port.on("searchHits", function(hits) {
+self.port.on("searchHitsDict", function(hitsDict) {
+	
+	currentTabUrl = hitsDict.url;
+	
 	$('body').empty();
+	$('body').append("<div id='sortContainer'><div id='bydate' class='sorter selected'>by date</div><div id='byrelevance' class='sorter unselected'>by relevance</div></div>");
 
-	for (hit in hits) {
+	for (hit in hitsDict.hits) {
 		
-		commentsLink = "<a href=https://news.ycombinator.com/item?id=" + hits[hit].objectID + " target='_blank'>  c: " + hits[hit].num_comments +"</a>";
-		titleLink = "<a href=" + hits[hit].url + " target='_blank'>" + hits[hit].title + "</a>";
-		authorLink = "<a href=" + "https://news.ycombinator.com/user?id=" + hits[hit].author + " target='_blank'> a: " + hits[hit].author + "</a>";
+		commentsLink = "<a href=https://news.ycombinator.com/item?id=" + hitsDict.hits[hit].objectID + " target='_blank'>  c: " + hitsDict.hits[hit].num_comments +"</a>";
+		titleLink = "<a href=" + hitsDict.hits[hit].url + " target='_blank'>" + hitsDict.hits[hit].title + "</a>";
+		authorLink = "<a href=" + "https://news.ycombinator.com/user?id=" + hitsDict.hits[hit].author + " target='_blank'> a: " + hitsDict.hits[hit].author + "</a>";	
 		
 		hitDiv = '<div class="containerDiv">' +
 			'<div class="titleDiv">' + titleLink + "</div>" +
 			'<div class="commentsDiv">' + commentsLink + '</div>' +
 			'<div class="authorDiv">' + authorLink + '| </div>' +
-			'<div class="dateDiv" id=' + hits[hit].objectID + '></div>'	+
+			'<div class="dateDiv" id=' + hitsDict.hits[hit].objectID + '></div>'	+
 			'</div>';
 		
 		$('body').append(hitDiv);
 		
 		//update date fields with more api requests
-		addDateToDateField(hits[hit].objectID);
+		addDateToDateField(hitsDict.hits[hit].objectID);
 	}
 });
 
-self.port.on("noHits", function(titleAndUrl) {
+self.port.on("nohits", function(titleAndUrl) {
 	$('body').empty();
 	submitUrl = "https://news.ycombinator.com/submitlink?u=" + 
 	titleAndUrl.url + 
@@ -42,4 +60,3 @@ self.port.on("noHits", function(titleAndUrl) {
 
 	$('body').append(linkToSubmit);
 });
- 
